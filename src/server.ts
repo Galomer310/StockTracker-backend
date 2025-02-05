b38d2c392284969ef519ef16bc8b80810baceb7b
Gal Omer <galomer6708@gmail.com>
2025-02-05 15:38:44 +0200

    packageJson tsconfig server

--- src/server.ts
+++ src/server.ts
@@ -5,37 +5,25 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes";
 import stockRoutes from "./routes/stockRoutes";
 import watchlistRoutes from "./routes/watchlistRoutes";
-import path from "path";
 
-
-
+// Load environment variables
 dotenv.config();
 
 // Initialize express
 const app = express();
 
-// 
+// Middleware
 app.use(express.json());
-
-// Enable CORS
 app.use(cors());
-
-// Cookie parser
 app.use(cookieParser());
 
-// Routes
+// API routes
 app.use("/auth", authRoutes);
 app.use("/stocks", stockRoutes);
 app.use("/watchlist", watchlistRoutes);
 
-// Serve static assets if in production
-app.use(express.static(path.join(__dirname, "../public")));
-
-// Handle React routing, return all requests to React app
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public", "index.html"));
-  });
-
-// Server
+// Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
