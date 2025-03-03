6512cddf4b722042c0728afb0caae7e09525947d
Gal Omer <galomer6708@gmail.com>
2025-03-03 16:16:50 +0200

    change jwt token exp time to 1h

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,11 +3,11 @@ import jwt from "jsonwebtoken";                 // Import jsonwebtoken for token
 // Function to generate an access token for a given user id
 export const generateAccessToken = (userId: number) => {
   // Sign a new token with the userId payload, secret from env variables, and expiration time of 15 minutes
-  return jwt.sign({ userId }, process.env.JWT_SECRET as string, { expiresIn: "15m" });
+  return jwt.sign({ userId }, process.env.JWT_SECRET as string, { expiresIn: "1h" });
 };
 
 // Generates a refresh token valid for 2 hours.
 export const generateRefreshToken = (userId: number) => {
-  // Refresh token with the userId payload, secret from env variables, and expiration time of 2 hours
-  return jwt.sign({ userId }, process.env.REFRESH_SECRET as string, { expiresIn: "2h" });
+  // Refresh token with the userId payload, secret from env variables, and expiration time of 4 hours
+  return jwt.sign({ userId }, process.env.REFRESH_SECRET as string, { expiresIn: "4h" });
 };
